eaa3fa5ddd4144574b0f3093a72106517b91155a
Addy Osmani <addyosmani@gmail.com>
2016-12-29 13:01:57 -0800

    (implementation): add devtools monkey patches

---
+++ src/devtools-monkeypatches.js
@@ -0,0 +1,15 @@
+/* global Runtime Common Bindings */
+
+Runtime.experiments = {};
+Runtime.experiments.isEnabled = exp => exp === 'timelineLatencyInfo';
+
+Common.moduleSetting = function(module) {
+  return {get: _ => module === 'showNativeFunctionsInJSProfile'};
+};
+
+// DevTools makes a few assumptions about using backing storage to hold traces.
+Bindings.DeferredTempFile = function() {};
+Bindings.DeferredTempFile.prototype = {
+  write: function() {},
+  finishWriting: function() {}
+};
